f844ffe13998b5322eb7b11323ddd94268f8d1a3
Renato <renatomercadoluna@hotmail.com>
2021-08-06 11:16:48 -0500

    repository module added

---
+++ data/repository/src/main/java/com/challenge/android_template/repository/FooRepository.kt
@@ -0,0 +1,28 @@
+package com.challenge.android_template.repository
+
+import com.challenge.android_template.api.FooApi
+import com.challenge.android_template.database.FooDb
+import com.challenge.android_template.model.Foo
+import javax.inject.Singleton
+
+/**
+ * Repository for [Foo]
+ */
+@Singleton
+class FooRepository(
+  private val fooApi: FooApi,
+  private val fooDb: FooDb
+) {
+
+  /**
+   * Generic method to interact with the local DB
+   */
+  suspend fun insert(foo: Foo) {
+    fooDb.insert(foo)
+  }
+
+  /**
+   * Generic method to interact with an API
+   */
+  suspend fun getAllFoos() = fooApi.getAllFoos()
+}
