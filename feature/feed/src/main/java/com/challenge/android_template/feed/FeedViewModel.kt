3cc53a1c13492d0b2c79fd79e1c0bbec25939669
Renato <renatomercadoluna@hotmail.com>
2021-11-01 19:35:40 -0500

    update feed module

---
+++ feature/feed/src/main/java/com/challenge/android_template/feed/FeedViewModel.kt
@@ -0,0 +1,10 @@
+package com.challenge.android_template.feed
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+/**
+ * [ViewModel] to store and manage Feed data.
+ */
+@HiltViewModel
+class FeedViewModel {
+}
